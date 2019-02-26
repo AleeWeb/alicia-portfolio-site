0049e7ff2b1ad618cef0e4dc14d5a30542ce0e3d
AleeWeb <aalexanderpr@gmail.com>
2019-02-26 13:26:20 -0500

    Updates

---
+++ precache-manifest.38c1bc6a148d660b74bf32c9f3ab2207.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f450ded07a45a70f9bb",
+    "url": "/alicia-portfolio-site/static/css/main.82d0bf3c.chunk.css"
+  },
+  {
+    "revision": "7f450ded07a45a70f9bb",
+    "url": "/alicia-portfolio-site/static/js/main.7f450ded.chunk.js"
+  },
+  {
+    "revision": "1ad8b546515e0a5e5a08",
+    "url": "/alicia-portfolio-site/static/js/runtime~main.1ad8b546.js"
+  },
+  {
+    "revision": "246c62dca49fd8378e7a",
+    "url": "/alicia-portfolio-site/static/css/2.abae74bc.chunk.css"
+  },
+  {
+    "revision": "246c62dca49fd8378e7a",
+    "url": "/alicia-portfolio-site/static/js/2.246c62dc.chunk.js"
+  },
+  {
+    "revision": "e065c0ebe4716e9ac210ff29f9117a22",
+    "url": "/alicia-portfolio-site/static/media/header_laptop.e065c0eb.svg"
+  },
+  {
+    "revision": "fe52409987a7dfdf1fcd1f49c2a3af86",
+    "url": "/alicia-portfolio-site/static/media/header_leftblob.fe524099.png"
+  },
+  {
+    "revision": "d23b3b7932f7053da9df32495f33db51",
+    "url": "/alicia-portfolio-site/static/media/cardthumb_djangohr.d23b3b79.png"
+  },
+  {
+    "revision": "e2393efd6390195fd179dbe3112ca1a5",
+    "url": "/alicia-portfolio-site/static/media/cardthumb_travelbuddy.e2393efd.png"
+  },
+  {
+    "revision": "8e07cdf07d00334e8bdd1bfd984435d7",
+    "url": "/alicia-portfolio-site/static/media/cardthumb_customerapi.8e07cdf0.png"
+  },
+  {
+    "revision": "456eb3160165d7c05e9a2bdda08c9e93",
+    "url": "/alicia-portfolio-site/static/media/cardthumb_openweather.456eb316.png"
+  },
+  {
+    "revision": "b512c62adf16c2d04c1d977f74f8ddad",
+    "url": "/alicia-portfolio-site/static/media/techreview.b512c62a.gif"
+  },
+  {
+    "revision": "a759390d17c9cc7bd9cc85191388419a",
+    "url": "/alicia-portfolio-site/static/media/mdptoolbox.a759390d.gif"
+  },
+  {
+    "revision": "db259a2ade6e133f0d5c6d2b775329e0",
+    "url": "/alicia-portfolio-site/index.html"
+  }
+];
