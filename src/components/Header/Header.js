d32dc4f66b501f0875c59c0b052f3899e235349e
Alicia Alexander <aalexanderpr@gmail.com>
2019-09-05 00:45:47 -0400

    Minor header text change

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -53,8 +53,8 @@ const Header = () => {
                 <Row>
                     <Col xs="9" sm="6" lg="5" style={headerTxtwrap}>
                         <h2 style={mainHeader}>Alicia Alexander</h2>
-                        <h3 style={bannerSubheader}>Front-End Designer and UI Developer</h3>
-                        <h5 className="phrase-header">I love to design prototypes and code!</h5>
+                        <h3 style={bannerSubheader}>Front-End Developer and Designer</h3>
+                        <h5 className="phrase-header">I love to design and code!</h5>
                     </Col>
 
                     <Col sm="6" lg="7">
