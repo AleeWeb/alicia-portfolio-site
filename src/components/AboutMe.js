4229b163df5798931271eec3278ab7c87789da83
Alicia <AleeWeb@users.noreply.github.com>
2020-06-11 01:06:50 -0400

    Updated About Me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -15,9 +15,8 @@ const AboutMe = () => {
 
                     <p>Hello. I have been working in web development since 2008. As technology has evolved, I have adapted and advanced with it.</p>
 
-                    <p>I primarily have experience working in the front-end over the years and I enjoy bringing design concepts to
-                        life through code. I entered the world of full stack development in the Fall of 2017 where I gained new experience with the back-end.
-                        My goal is to always keep growing as a professional.</p>
+                    <p>I primarily have experience with design and working in the front-end over the years and I enjoy bringing design concepts to
+                        life through code. My goal is to always keep growing as a professional.</p>
 
                 </Col>
                 </Row>
